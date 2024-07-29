53202e8ec4373ea793af26d07db6591b895b4bc1
AKHIL BABU <akhilbabui1998@gmail.com>
2024-07-29 12:51:36 +0530

    Use js to create the radio buttons.

--- js/manage_display.js
+++ js/manage_display.js
@@ -2,16 +2,27 @@
  * @file
  * Field Display Manager behaviors.
  */
-(function (Drupal) {
+(function ($, Drupal, once) {
 
   'use strict';
 
   Drupal.behaviors.fieldDisplayManagerManageDisplay = {
     attach (context, settings) {
+      // Create the radio buttons.
+      Drupal.theme.manageDisplayToggle = () =>
+        `<div class="manage-display-toggle-wrapper" data-drupal-selector="manage-display-toggle-wrapper" style="display: inline-block;">
+          <input type="radio" id="display-toggle-enable-all" name="manage_display_toggle" value="enable_all">
+          <label for="display-toggle-enable-all">${Drupal.t('Enable all fields')}</label>
+          <input type="radio" id="display-toggle-disable-all" name="manage_display_toggle" value="disable_all">
+          <label for="display-toggle-disable-all">${Drupal.t('Disable all fields')}</label>
+        </div>`;
 
-      console.log('It works!');
-
+      const $wrapper = $(once("tabledragToggle", ".tabledrag-toggle-weight-wrapper", context));
+      const $button = $wrapper.find("button.tabledrag-toggle-weight");
+      const $radios = $(Drupal.theme('manageDisplayToggle'));
+      // Insert the radio buttons.
+      $button?.before($radios);
     }
   };
 
-} (Drupal));
+} (jQuery, Drupal, once));
