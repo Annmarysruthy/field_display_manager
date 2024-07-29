9940ed003340d076fe7c17706d23d48f1641660b
AKHIL BABU <akhilbabui1998@gmail.com>
2024-07-29 11:02:31 +0530

    Update radio field and add js.

---
+++ js/manage_display.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Field Display Manager behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fieldDisplayManagerManageDisplay = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
